65edae7b841dca6628691640f0a7a9b090afd0ef
njihiamark <njihiamark@gmail.com>
2018-08-27 17:31:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import HelloWorld from "./HelloWorld";
+
+class App extends Component {
+  state = {
+    tech: "Redux"
+  };
+  render() {
+    return <HelloWorld tech={this.state.tech} />;
+  }
+}
+
+export default App;
